9a0d94eaa260eb9bf6cf698ccb4d98d8428b6e46
Oleg <slepcov_oleg@mail.ru>
2022-07-04 01:18:23 +0700

    Воврат асинронными задачами результатов выполнения

--- reportdpk/api_v1/tasks.py
+++ reportdpk/api_v1/tasks.py
@@ -5,24 +5,24 @@ from .services.tasks import company, stages, directions, deal, calls
 
 @celery_app.task
 def directions_create_or_update():
-    directions.create_or_update()
+    return directions.create_or_update()
 
 
 @celery_app.task
 def stages_create_or_update(id_direction):
-    stages.create_or_update(id_direction)
+    return stages.create_or_update(id_direction)
 
 
 @celery_app.task
 def company_create_or_update(id_company):
-    company.create_or_update(id_company)
+    return company.create_or_update(id_company)
 
 
 @celery_app.task
 def deal_create_or_update(id_deal):
-    deal.create_or_update(id_deal)
+    return deal.create_or_update(id_deal)
 
 
 @celery_app.task
 def calls_create_or_update(id_call):
-    calls.create_or_update(id_call)
+    return calls.create_or_update(id_call)
