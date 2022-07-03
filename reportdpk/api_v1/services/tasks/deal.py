0087ee067953c8de77dedcc0733c818efa38efcc
Oleg <slepcov_oleg@mail.ru>
2022-07-04 01:28:38 +0700

    Исправлено поле сохранения даты

--- reportdpk/api_v1/services/tasks/deal.py
+++ reportdpk/api_v1/services/tasks/deal.py
@@ -31,7 +31,7 @@ def create_or_update(id_deal):
         "title": response_deal["result"]["TITLE"],
         "date_create": response_deal["result"]["DATE_CREATE"],
         "date_modify": response_deal["result"]["DATE_MODIFY"],
-        "date_closed": response_deal["result"]["CLOSEDATE"],
+        "date_closed": response_deal["result"]["CLOSEDATE"] or None,
         "closed": True if response_deal["result"]["CLOSED"] == "Y" else False,
         "opportunity": response_deal["result"]["OPPORTUNITY"],
         "balance_on_payments": editing_numb(response_deal["result"]["UF_CRM_1575629957086"]),
