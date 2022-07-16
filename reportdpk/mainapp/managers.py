bc0128f015a17c41aa7a56d3f20a41e26531988f
Oleg <slepcov_oleg@mail.ru>
2022-07-16 23:26:04 +0700

    1

--- reportdpk/mainapp/managers.py
+++ reportdpk/mainapp/managers.py
@@ -133,7 +133,7 @@ class DealManager(models.Manager):
             summa_by_company_success=models.Subquery(
                 Deal.objects.filter(
                     company=models.OuterRef('company__pk'),
-                    stage__status="SUCCESSFUL"
+                    # stage__status="SUCCESSFUL"
                 ).annotate(
                     s=models.Sum('opportunity')
                 ).values('s')[:1]
@@ -141,7 +141,7 @@ class DealManager(models.Manager):
             summa_by_company_work=models.Subquery(
                 Deal.objects.filter(
                     company=models.OuterRef('company__pk'),
-                    stage__status="WORK"
+                    # stage__status="WORK"
                 ).annotate(
                     s=models.Sum('opportunity')
                 ).values('s')[:1]
