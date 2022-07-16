6fc2d007bc72b1ae52843183da5d8b4dc204dddd
Oleg <slepcov_oleg@mail.ru>
2022-07-16 23:17:58 +0700

    1

--- reportdpk/mainapp/managers.py
+++ reportdpk/mainapp/managers.py
@@ -130,21 +130,21 @@ class DealManager(models.Manager):
                         s=models.Sum('opportunity')
                     ).values('s')[:1]
                 ),
-            # summa_by_company_success=models.Subquery(
-            #     Deal.objects.filter(
-            #         company=models.OuterRef('company__pk'),
-            #         stage__status="SUCCESSFUL"
-            #     ).aggregate(
-            #         s=models.Sum('opportunity')
-            #     )['s']
-            # ),
+            summa_by_company_success=models.Subquery(
+                Deal.objects.filter(
+                    company=models.OuterRef('company__pk'),
+                    stage__status="SUCCESSFUL"
+                ).aggregate(
+                    s=models.Sum('opportunity')
+                )['s']
+            ),
             summa_by_company_work=models.Subquery(
                 Deal.objects.filter(
                     company=models.OuterRef('company__pk'),
                     stage__status="WORK"
                 ).annotate(
                     s=models.Sum('opportunity')
-                ).values('s')
+                ).values('s')[:1]
             ),
             # models.Sum("opportunity", filter=models.Q(stage__status="WORK")),
         )
