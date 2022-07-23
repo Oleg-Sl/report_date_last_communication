f99cf8097a311c7c98b7a7aea1ef59fdaa21f108
Oleg <slepcov_oleg@mail.ru>
2022-07-23 23:23:30 +0700

    Добавлен запрос для получения суммы текущих и закрытых сделок по компании

--- reportdpk/mainapp/managers.py
+++ reportdpk/mainapp/managers.py
@@ -199,9 +199,10 @@ class CompanyNewManager(models.Manager):
                     s=models.Sum('opportunity')
                 ).values('s')[:1]
             ),
-        ).values(
-            "pk", "summa_by_company_success", "summa_by_company_work"
         )
+            # .values(
+            # "pk", "summa_by_company_success", "summa_by_company_work"
+        # )
 
 
 
